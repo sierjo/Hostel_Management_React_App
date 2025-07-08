4df4a3184edc8972100ff59c474523665487aa3c
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-07-08 08:30:00 +0300

    feat: set up main App component with routing and layout structure.
    - Created base structure of the App component using React Router (BrowserRouter, Routes, Route).
    - Integrated Navbar and FooterComponent for consistent layout across pages.
    - Added HomePage component and connected it to the route path '/home'.
    - Imported and rendered global styles from App.css.
    - Imported and used logo.svg.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Navbar from './componemt/common/Foother';
+import HomePage from './componemt/home/HomePg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FooterComponent from './componemt/common/Foother';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route exact path='/home' element={<HomePage />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </div>
+    </BrowserRouter>
   );
 }
 
