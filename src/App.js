c5409905e9b6e510088bd909e6cd0e0f0e789f60
Ushkevich Siarhei <siarheiushkevich@gmail.com>
2025-07-09 16:23:44 +0300

    feat: implement room listing and detailed booking pages with routing
    - Added AllRoomsPage component to display all rooms with:
      - Room type filtering
      - Integrated RoomSearch and RoomResult components for modularity
    - Added RoomDetailsPage component to show details of a selected room and allow bookings
      - Integrated booking form with check-in/out date pickers and guest number inputs
      - Displayed existing bookings for the room
      - Calculated total price dynamically based on number of nights
      - Submitted booking and displayed confirmation message
    - Added routing to App component:
      - /rooms → AllRoomsPage
      - /roon-details-book/:roomId → RoomDetailsPage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import HomePage from './componemt/home/HomePg';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FooterComponent from './componemt/common/Foother';
 import FindBookingPage from './componemt/booking_rooms/FindBookingPage';
+import RoomDetailsPage from './componemt/booking_rooms/RoomDetailsPage';
+import AllRoomPage from './componemt/booking_rooms/AllRoomPage';
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +16,8 @@ function App() {
         <div className='content'>
           <Routes>
             <Route exact path='/home' element={<HomePage />} />
+            <Route exact path='/rooms' element={<AllRoomPage />} />
+            <Route path='/roon-details-book/:roomId' element={<RoomDetailsPage />} />
             <Route path='/find-booking' element={<FindBookingPage />} />
           </Routes>
         </div>
